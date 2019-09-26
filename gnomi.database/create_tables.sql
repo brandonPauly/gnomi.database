27fc06ae3376a17327e0693dea774d8c9a24bee8
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-09-25 20:17:25 -0500

    adding verificationKey table creation

--- gnomi.database/create_tables.sql
+++ gnomi.database/create_tables.sql
@@ -5,19 +5,33 @@ GO
 USE gnomi_dev;
 
 
-CREATE TABLE [dbo].[gender](
-[genderId] [tinyint] IDENTITY(1,1) NOT NULL,
-[genderName] [nvarchar](150),
-CONSTRAINT [PK_gender] PRIMARY KEY ([genderId]));
+CREATE TABLE [dbo].[gender]
+(
+	[genderId] [tinyint] IDENTITY(1,1) NOT NULL,
+	[genderName] [nvarchar](150),
+	CONSTRAINT [PK_gender] PRIMARY KEY ([genderId])
+);
 
-CREATE TABLE [dbo].[human](
-[humanId] [bigint] IDENTITY(1,1) NOT NULL,
-[email] [nvarchar](254),
-[password] [nvarchar](500) NOT NULL,
-[firstName] [nvarchar](120) NOT NULL,
-[lastName] [nvarchar](150) NOT NULL,
-[dateOfBirth] [datetime] NOT NULL,
-[genderId] [tinyint] NOT NULL,
-[zipcode] [nvarchar](30)
-CONSTRAINT [PK_human] PRIMARY KEY CLUSTERED ([humanId])
-CONSTRAINT [FK_genderId] FOREIGN KEY ([genderId]) REFERENCES [dbo].[gender] ON UPDATE CASCADE);
+CREATE TABLE [dbo].[human]
+(
+	[humanId] [bigint] IDENTITY(1,1) NOT NULL UNIQUE,
+	[email] [nvarchar](254) NOT NULL UNIQUE,
+	[password] [nvarchar](500) NOT NULL,
+	[firstName] [nvarchar](120),
+	[lastName] [nvarchar](150),
+	[dateOfBirth] [datetime],
+	[genderId] [tinyint],
+	[zipcode] [nvarchar](30),
+	[signUpDate] [DateTime],
+	[isVerified] [bit],
+	CONSTRAINT [PK_human] PRIMARY KEY CLUSTERED ([humanId]),
+	CONSTRAINT [FK_genderId] FOREIGN KEY ([genderId]) REFERENCES [dbo].[gender] ON UPDATE CASCADE
+);
+
+CREATE TABLE [dbo].[verificationKey]
+(
+	[humanId] [bigint] NOT NULL UNIQUE,
+	[verificationGuid] [nvarchar](60) NOT NULL,
+	CONSTRAINT [PK_verificationKey] PRIMARY KEY ([humanId], [verificationGuid]),
+	CONSTRAINT [FK_humanId] FOREIGN KEY ([humanId]) REFERENCES [dbo].[human] ON UPDATE CASCADE
+);
