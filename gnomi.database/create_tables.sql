38f7e7a7b09bd7b7b020a2fb4f7d7ec226d97d68
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-11-13 22:03:35 -0600

    updated length for verification code; added initiation date for verification

--- gnomi.database/create_tables.sql
+++ gnomi.database/create_tables.sql
@@ -22,7 +22,7 @@ CREATE TABLE [dbo].[human]
 	[dateOfBirth] [datetime],
 	[genderId] [tinyint],
 	[zipcode] [nvarchar](30),
-	[signUpDate] [DateTime],
+	[signUpDate] [datetime],
 	[isVerified] [bit],
 	CONSTRAINT [PK_human] PRIMARY KEY CLUSTERED ([humanId]),
 	CONSTRAINT [FK_genderId] FOREIGN KEY ([genderId]) REFERENCES [dbo].[gender] ON UPDATE CASCADE
@@ -31,7 +31,8 @@ CREATE TABLE [dbo].[human]
 CREATE TABLE [dbo].[verificationKey]
 (
 	[humanId] [bigint] NOT NULL UNIQUE,
-	[verificationCode] [nvarchar](60) NOT NULL,
+	[verificationCode] [nvarchar](96) NOT NULL,
+	[initiationDate] [datetime] NOT NULL,
 	CONSTRAINT [PK_verificationKey] PRIMARY KEY ([humanId], [verificationCode]),
 	CONSTRAINT [FK_humanId] FOREIGN KEY ([humanId]) REFERENCES [dbo].[human] ON UPDATE CASCADE
 );
